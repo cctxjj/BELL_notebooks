010bd94a505b75ba744c13fd8e697866a2e38487
cctxjj <sebastian.dietrich7@outlook.de>
2025-05-04 19:30:21 +0200

    Full Bresenham done Todo: write concrete showcase of algorithm, adjust existing variants

---
+++ util.py
@@ -0,0 +1,142 @@
+from debugpy.server.cli import switches
+from ipycanvas import Canvas
+from IPython.display import display
+import matplotlib.pyplot as plt
+
+def zeichne_pixel(punkte, pixelgröße=10, rand=20):
+    """
+    Zeichnet ein Koordinatensystem mit eingezeichneten Achsen und stellt gegebene Punkte als 10x10 Pixel große schwarze Quadrate dar.
+
+    :param punkte: Liste von (x, y)-Tupeln, z. B. [(1, 2), (3, 4)]
+    :param pixelgröße: Größe jedes Punkts in Pixeln (Standard: 10)
+    :param rand: Rand in Pixeln um das Koordinatensystem (Standard: 20)
+    """
+    if not punkte:
+        print("Die Punkteliste ist leer.")
+        return
+
+    # Bestimme die minimalen und maximalen x- und y-Werte
+    min_x = min(x for x, _ in punkte)
+    max_x = max(x for x, _ in punkte)
+    min_y = min(y for _, y in punkte)
+    max_y = max(y for _, y in punkte)
+
+    # Berechne die Breite und Höhe des Koordinatensystems
+    breite = (max_x - min_x + 1) * pixelgröße + 2 * rand
+    höhe = (max_y - min_y + 1) * pixelgröße + 2 * rand
+
+    # Erstelle das Canvas
+    canvas = Canvas(width=breite, height=höhe)
+
+    for x, y in punkte:
+        # Transformiere die Koordinaten in Canvas-Koordinaten
+        canvas_x = rand + (x - min_x) * pixelgröße
+        canvas_y = höhe - rand - (y - min_y) * pixelgröße - pixelgröße
+        canvas.fill_style = 'black'
+        canvas.fill_rect(canvas_x, canvas_y, pixelgröße, pixelgröße)
+
+    display(canvas)
+
+def plot_points(points):
+    """
+    Zeichnet eine Liste von (x, y)-Tupeln als schwarze Quadrate in einem dynamisch angepassten Koordinatensystem.
+
+    Parameters:
+    points (list of tuples): Liste der zu zeichnenden Punkte, z. B. [(x1, y1), (x2, y2), ...]
+    """
+    if not points:
+        print("Die Punkteliste ist leer.")
+        return
+
+    # Extrahiere x- und y-Koordinaten
+    x_coords, y_coords = zip(*points)
+
+    # Erstelle die Grafik
+    fig, ax = plt.subplots()
+
+    # Zeichne die Punkte als schwarze Quadrate
+    ax.scatter(x_coords, y_coords, s=100, c='black', marker='s')  # s=100 entspricht etwa 10x10 Pixeln
+
+    # Achsenbeschriftungen
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+
+    # Gitterlinien hinzufügen
+    ax.grid(True)
+
+    # Achsenlimits dynamisch anpassen mit etwas Puffer
+    x_min, x_max = min(x_coords), max(x_coords)
+    y_min, y_max = min(y_coords), max(y_coords)
+    x_range = x_max - x_min
+    y_range = y_max - y_min
+    ax.set_xlim(x_min - 0.1 * x_range, x_max + 0.1 * x_range)
+    ax.set_ylim(y_min - 0.1 * y_range, y_max + 0.1 * y_range)
+
+    # Achsen gleich skalieren
+    ax.set_aspect('equal', adjustable='box')
+
+    # Plot anzeigen
+    plt.show()
+
+def bresenham(x_0, y_0, x_1, y_1):
+    if x_0 > x_1:
+        x_0, x_1 = x_1, x_0
+        y_0, y_1 = y_1, y_0
+
+    d_x = abs(x_1 - x_0)
+    d_y = abs(y_1 - y_0) # add check for zero division
+    m = d_y/d_x
+    if m < 1:
+        f_dir = x_0
+        s_dir = y_0
+
+        d_n = 2*d_y - d_x
+
+        result = [(f_dir, s_dir)]
+        step = 1 if y_0 < y_1 else -1
+
+        while f_dir < x_1:
+            f_dir += 1
+            if d_n < 0:
+                d_n += 2 * d_y
+            else:
+                s_dir += step
+                d_n += 2 * (d_y - d_x)
+            result.append((f_dir, s_dir))
+        return result
+    else:
+        f_dir = y_0
+        s_dir = x_0
+
+        d_x, d_y = d_y, d_x
+
+        d_n = 2 * d_y - d_x
+
+
+        result = [(s_dir, f_dir)]
+        if y_0 < y_1:
+            while f_dir < y_1:
+                f_dir += 1
+                if d_n < 0:
+                    d_n += 2 * d_y
+                else:
+                    s_dir += 1
+                    d_n += 2 * (d_y - d_x)
+                result.append((s_dir, f_dir))
+
+            return result
+
+        else:
+            while f_dir > y_1:
+                f_dir -= 1
+                if d_n < 0:
+                    d_n += 2 * d_y
+                else:
+                    s_dir += 1
+                    d_n += 2 * (d_y - d_x)
+                result.append((s_dir, f_dir))
+
+            return result
+
+
+bresenham(0, 0, 10, 5)
