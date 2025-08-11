9f542d1e21ccb688ed6a91e4739fe97f5528bb1e
cctxjj <sebastian.dietrich7@outlook.de>
2025-08-11 22:08:37 +0200

    Added basis for dda line drawing, to be finished

--- area_fill/fill_algs.py
+++ area_fill/fill_algs.py
@@ -339,7 +339,92 @@ def scanline_stackbased_flood_fill_4con(
     :return 2D-Array representing the modified picture
     '''
     height, width = np.shape(img)
-    start_col = img[start[1], start[0]] # switched weil kp
+    start_col = img[start[1], start[0]]
+
+    stack = [start]
+
+    def identify_and_fill_run(seed):
+        img[seed[1], seed[0]] = new_color
+        left_b, y = seed[0]-1, seed[1]
+        right_b = left_b+2
+
+        run_above_detected_on_start = False
+        run_below_detected_on_start = False
+        if y + 1 < height:
+            if img[cur[1] + 1, cur[0]] == start_col:
+                stack.append((cur[0], cur[1] + 1))
+                run_above_detected_on_start = True
+        if y - 1 >= 0:
+            if img[cur[1] - 1, cur[0]] == start_col:
+                stack.append((cur[0], cur[1] - 1))
+                run_below_detected_on_start = True
+
+        # looking for the left boundary, scanning for new runs above/below
+        run_above = run_above_detected_on_start
+        run_below = run_below_detected_on_start
+        while img[y, left_b] == start_col and left_b > 0:
+            img[y, left_b] = new_color
+
+            if y + 1 < height:
+                if run_above:
+                    if img[y + 1, left_b] != start_col:
+                        run_above = False
+                elif img[y + 1, left_b] == start_col:
+                    run_above = True
+                    stack.append((left_b, y + 1))
+
+            if y - 1 >= 0:
+                if run_below:
+                    if img[y - 1, left_b] != start_col:
+                        run_below = False
+                elif img[y - 1, left_b] == start_col:
+                    run_below = True
+                    stack.append((left_b, y - 1))
+            left_b -= 1
+
+        # looking for the right boundary, scanning for new runs above/below
+        run_above = run_above_detected_on_start
+        run_below = run_below_detected_on_start
+        while img[y, right_b] == start_col and right_b < width-1:
+            img[y, right_b] = new_color
+
+            if y + 1 < height:
+                if run_above:
+                    if img[y + 1, right_b] != start_col:
+                        run_above = False
+                elif img[y + 1, right_b] == start_col:
+                    run_above = True
+                    stack.append((right_b, y + 1))
+
+            if y - 1 >= 0:
+                if run_below:
+                    if img[y - 1, right_b] != start_col:
+                        run_below = False
+                elif img[y - 1, right_b] == start_col:
+                    run_below = True
+                    stack.append((right_b ,y - 1))
+
+            right_b += 1
+
+    while stack:
+        cur = stack.pop()
+        identify_and_fill_run(cur)
+
+    return img
+
+def scanline_stackbased_boundary_fill_4con(
+        img: np.ndarray,
+        start: tuple,
+        new_color: int):
+    '''
+    implementation of the boundary fill algorithm using 4-connectedness and the concept of scanlines/runs
+    :param img: 2D-Array with ints representing greyscale values from 0 to 255
+    :param start: tuple representing starting point P(x|y)
+    :param new_color: desired new color to replace old one at P(x|y)
+    :return 2D-Array representing the modified picture
+    '''
+    height, width = np.shape(img)
+    start_col = img[start[1], start[0]]
 
     stack = [start]
 
--- rasterisation/rasterisation_algs.py
+++ rasterisation/rasterisation_algs.py
@@ -1,14 +1,39 @@
 # TODO: Add DDA --> compare; add circle (Midpoint, Bresenham) & compare
-
-def bresenham(x_0, y_0, x_1, y_1):
-    # calculates straight points from Point A(x_0, y_0) to B(x_1, y_1) according to J. E. Bresenham
-    # Time-complexity: O(n)
-    # Space-complexity: O(1) --> not using floating point operations
+def dda_line(x_0, y_0, x_1, y_1):
     if x_0 > x_1:
         x_0, x_1 = x_1, x_0
         y_0, y_1 = y_1, y_0
 
-    d_x = abs(x_1 - x_0) # TODO: Warum muss hier abs hin? --> Antort: Weil negativwerte die Fehlerrechnung mit d_y verzerren, Fehlerrechnung funktioniert unabhängig von Richtung gleich --> mathematischen Hintergrund betrachten
+    m = (y_1 - y_0)/(x_1 - x_0)
+    print(m)
+
+    result = []
+    cur_x = x_0
+    cur_y = y_0
+    inc_x = 1
+    inc_y = m
+    if abs(m) > 1:
+        m = 1/m
+        inc_x, inc_y = m, inc_x
+
+    while abs(cur_x) < x_1 and abs(cur_y) < y_1:
+        cur_x += inc_x
+        cur_y += inc_y
+        result.append((round(cur_x, 0), round(cur_y, 0)))
+    # TODO: Fix
+    return result
+
+
+def bresenham(x_0, y_0, x_1, y_1):
+    # calculates straight points from Point A(x_0, y_0) to B(x_1, y_1) according to J. E. Bresenham
+    # time complexity: O(n)
+    # space complexity: O(1)
+    # --> not using floating point operations
+    if x_0 > x_1:
+        x_0, x_1 = x_1, x_0
+        y_0, y_1 = y_1, y_0
+
+    d_x = abs(x_1 - x_0) # TODO: Warum muss hier abs hin? --> Antwort: Weil negativwerte die Fehlerrechnung mit d_y verzerren, Fehlerrechnung funktioniert unabhängig von Richtung gleich --> mathematischen Hintergrund betrachten
     d_y = abs(y_1 - y_0) # check for zero division
 
     if d_x > d_y:
