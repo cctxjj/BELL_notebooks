0ea7b87f4ffcaedb130a8f10bb98700d1124739b
cctxjj <sebastian.dietrich7@outlook.de>
2025-10-14 20:41:45 +0200

    try 3 (test 2)

--- util/datasets/create_ds_1.py
+++ util/datasets/create_ds_1.py
@@ -2,8 +2,8 @@ from util.datasets.dataset_creator import create_fixed_len_bez_curves_drag_coef_
 
 from aerosandbox import XFoil, Airfoil
 af = Airfoil("naca2412")
-xf = XFoil(airfoil=af, verbose=True)
-res = xf.alpha(5)
+xf = XFoil(airfoil=af, verbose=False)
+res = xf.alpha(2)
 print(res)
 
 length = int(input("Enter desired length of the dataset: "))
