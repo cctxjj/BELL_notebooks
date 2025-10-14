85e1a610733f0da3d7fb186e706c3aebdfd0e85f
cctxjj <sebastian.dietrich7@outlook.de>
2025-10-14 21:12:45 +0200

    try 3 (test 4)

--- util/datasets/create_ds_1.py
+++ util/datasets/create_ds_1.py
@@ -1,9 +1,17 @@
+import os
+import tempfile
+
 from util.datasets.dataset_creator import create_fixed_len_bez_curves_drag_coef_dataset
 
 from aerosandbox import XFoil, Airfoil
-af = Airfoil("naca2412")
-xf = XFoil(airfoil=af, verbose=False)
+
+print("test0")
+tempfile.TemporaryDirectory()
 print("test1")
+af = Airfoil("naca2412")
+print("test2")
+xf = XFoil(airfoil=af, verbose=False)
+print("test3")
 res = xf.alpha(2)
 print(res)
 
