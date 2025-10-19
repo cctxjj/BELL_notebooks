8a0825cb66dc3f7631da1652a27e645451cd3691
cctxjj <sebastian.dietrich7@outlook.de>
2025-10-19 21:59:51 +0200

    Started Neural Foil Implementation

--- curves/neural/custom_metrics/drag_evaluation.py
+++ curves/neural/custom_metrics/drag_evaluation.py
@@ -1,20 +1,20 @@
 # Todo: Idee: Nurb-Surface --> Kurvenpunkte werden jeweils interpoliert, custom metrik --> Nurb macht maybe für Kurve keinen sinn, aber für Fläche
 import math
 import os
-import random
 import sys
 
 import numpy as np
 
 import util.graphics.visualisations as vis
 from aerosandbox import Airfoil, XFoil
+import neuralfoil as nf
 
 from curves.func_based.bézier_curve import bezier_curve
 from util.shape_modifier import normalize_points, converge_shape_to_mirrored_airfoil
 
 eval_count = 0
-#default_path = "C:\\Users\\Sebastian\\PycharmProjects\BELL_notebooks/data/neural_curves/airfoil_data_nomenclat2"
-default_path = "/root/bell/data/neural_curves/airfoil_on_server"
+default_path = "C:\\Users\\Sebastian\\PycharmProjects\BELL_notebooks/data/neural_curves/airfoil_data_nomenclat2"
+#default_path = "/root/bell/data/neural_curves/airfoil_on_server"
 
 class DragEvaluator:
     def __init__(self,
@@ -25,7 +25,6 @@ class DragEvaluator:
                  specification: str = None):
         global eval_count
         eval_count+=1
-
         # obj attributes
         self.name = f"{specification}_af_evaluation_{eval_count}" if specification is not None else f"af_evaluation_{eval_count}"
         self.range = range
@@ -37,9 +36,17 @@ class DragEvaluator:
         self.rotation = translation[1] # TODO: use rotation data to adjust angle of incoming airstream
         af_points = translation[0]
         af_points = np.array(af_points)
+        # asb.Airfoil
 
         # setup airfoil obj
         self.airfoil = Airfoil(coordinates=af_points, name=self.name).repanel(n_points_per_side=200)
+        print(
+            nf.get_aero_from_airfoil(
+                airfoil=self.airfoil,
+                Re=10e6,
+                alpha=0,
+            )
+        )
         #self.airfoil = Airfoil(name="naca0012")
 
         if save_airfoil:
@@ -47,6 +54,8 @@ class DragEvaluator:
 
             vis.visualize_curve(points=points, save_path=save_dir, file_name="curve.png")
             vis.visualize_curve(points=self.airfoil.coordinates, save_path=save_dir, file_name="airfoil.png")
+
+# TODO: rewrite to fit NeuralFoil
 
     def execute(
             self,
@@ -62,9 +71,8 @@ class DragEvaluator:
             Re=re,
             verbose=False,
             xfoil_repanel=True
-            # todo: path anpassen
         )
-
+        print(xf.alpha(0).get("CD"))
         xf.timeout = 1000000
         alphas = [*range(self.start_angle+self.rotation, self.start_angle+self.range+self.rotation)]
         cds = {}
@@ -112,7 +120,7 @@ class DragEvaluator:
 
 
 
-#cont_points = [(0, 0), (0.5, 1.5), (3, 2), (10, 0.5), (11, 0)]
-#curve_points = bezier_curve(cont_points, 50)
-#ev = DragEvaluator(curve_points, save_airfoil=False, range=30, start_angle=0, name_appendix="custom_drag_test_naca0012")
-#print(ev.execute())
+cont_points = [(0, 0), (0.5, 1.5), (3, 2), (10, 0.5), (11, 0)]
+curve_points = bezier_curve(cont_points, 250)
+ev = DragEvaluator(curve_points, save_airfoil=False, range=30, start_angle=0, specification="custom_drag_test_naca0012")
+print(ev.execute())
