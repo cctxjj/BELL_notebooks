afc1f1e39c55411d6117db6dcb2338f4fafb8dc1
cctxjj <sebastian.dietrich7@outlook.de>
2025-11-13 16:43:08 +0100

    added second file

---
+++ util/datasets/create_ds_2.py
@@ -0,0 +1,9 @@
+import os
+import tempfile
+
+from util.datasets.dataset_creator import create_bez_curves_drag_coef_dataset
+
+length = int(input("Enter desired length of the dataset: "))
+n1, n2 = create_bez_curves_drag_coef_dataset(length=length, file_name="bez_curves_afs_cd_vals_2")
+print("done :)")
+print(n1, n2)
