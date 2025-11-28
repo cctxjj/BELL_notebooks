ff326ac7da8e9e712591580cb5352e647bc3b627
cctxjj <sebastian.dietrich7@outlook.de>
2025-11-28 17:09:00 +0100

    remodeled to drag factor --> improvement

--- curves/neural/purposebased/regressors/linear_regression_drag_to_improvement.py
+++ curves/neural/purposebased/regressors/linear_regression_drag_to_improvement.py
@@ -12,7 +12,7 @@ import pandas as pd
 spec = input("csv specification: ")
 df = pd.read_csv(f"C:\\Users\\Sebastian\\PycharmProjects\\BELL_notebooks\\data\\model_analysis_1\\crossmodel_analysis_{spec}.csv")
 
-to_predict = float(input("target drag improvement: "))
+to_predict = float(input("drag factor: "))
 
 n = df["n"].tolist()
 drag_improvement = df["drag_improvement"].tolist()
@@ -20,19 +20,19 @@ bez_shift = df["bezier_shift"].tolist()
 
 ratio = df["ratio"].tolist()
 
-x_train, x_test, y_train, y_test = train_test_split(np.reshape(drag_improvement, (-1, 1)), n, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(np.reshape(n, (-1, 1)), drag_improvement, test_size=0.2)
 
 regressor = LinearRegression()
 regressor.fit(x_train, y_train)
 print("Score: " + str(regressor.score(x_test, y_test)))
 concrete_prediction = regressor.predict(np.reshape([to_predict], (-1, 1)))[0]
-print(f"Predicted drag factor for drag improvement={to_predict}: {concrete_prediction}")
+print(f"Predicted drag improvement for drag factor={to_predict}: {concrete_prediction}")
 
-general_prediction = regressor.predict(np.reshape([x for x in range(0, 2)], (-1, 1)))
+general_prediction = regressor.predict(np.reshape([0, 30], (-1, 1)))
 
 # plot prediction for drag factor
 plt.figure(figsize=(8, 4))
-plt.plot([x for x in range(0, 2)], general_prediction, color="purple",
+plt.plot([0, 30], general_prediction, color="purple",
     linewidth=2.0,
     alpha=0.8,
     solid_joinstyle="round",
@@ -40,10 +40,10 @@ plt.plot([x for x in range(0, 2)], general_prediction, color="purple",
     antialiased=True,
     zorder=2,
   label="vorhergesagte Modelperformance")
-plt.scatter(drag_improvement, n, color="red", label="reale Modelperformance")
-plt.xlabel("Drag Improvement")
-plt.ylabel("Drag Faktor")
-plt.title("Drag Faktor nach Drag Improvement")
+plt.scatter(n, drag_improvement, color="red", label="reale Modelperformance")
+plt.ylabel("Drag Improvement")
+plt.xlabel("Drag Faktor")
+plt.title("Drag Improvement nach Drag Faktor")
 plt.grid(True, alpha=0.3)
 plt.legend()
 plt.tight_layout()
